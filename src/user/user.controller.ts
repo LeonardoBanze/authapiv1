cd0017223d74006ae04ff95455c5706f216651ca
Leonardo Banze <leonardobanze37@gamil.com>
2024-10-21 21:46:09 +0200

    feate: add CRUD

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,15 @@
 import { Prisma, User } from '@prisma/client';
 import { UserService } from './user.service';
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { User as UserModel } from '@prisma/client';
 
 @Controller('user')
@@ -13,5 +22,26 @@ export class UserController {
     @Body() userData: Prisma.UserCreateInput,
   ): Promise<UserModel> {
     return this.UserService.createUser(userData);
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<UserModel> {
+    return this.UserService.user({ id: Number(id) });
+  }
+
+  @Put()
+  async updateUser(
+    @Body() userData: Prisma.UserUpdateInput,
+    @Param('id') id: string,
+  ): Promise<UserModel> {
+    return this.UserService.updateUser({
+      where: { id: Number(id) },
+      data: userData,
+    });
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.UserService.deleteUser({ id: Number(id) });
   }
 }
